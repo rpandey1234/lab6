2dfbbfe0db2f227155c88e4592ac29166320dbf3
Rahul Pandey <rpandey1234@gmail.com>
2014-02-12 15:14:40 -0800

    done with basic lab, ajax

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,8 +25,17 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+	var detailsDiv = this;
 
 	console.log("User clicked on project " + idNumber);
+	$.get('/project/' + idNumber, function(data){
+		console.log(data);
+		var detailHtml = "<p>" + data['title'] + "</p>"
+			+ "<p>" + data['date'] + "</p>"
+			+ "<img class='detailsImage' src='" + data['image'] + "'></img>"
+			+ "<p>" + data['summary'] + "</p>";
+		$(detailsDiv).parent().find('.details').html(detailHtml);
+	});
 }
 
 /*
@@ -35,4 +44,13 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get('/palette', function(data){
+		console.log(data);
+		var colors = data['colors'].hex;
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	});
 }
